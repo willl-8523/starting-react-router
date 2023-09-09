71a1316d9874678b497887bb83bbc35d7f95dc4f
willl85 <ngowilfried@gmail.com>
2023-09-09 16:30:12 +0200

    Create Products Page Component and the second route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Homepage from './pages/Home';
+import ProductsPage from './pages/Products';
 
-const router = createBrowserRouter([{ path: '/', element: <Homepage /> }]);
+const router = createBrowserRouter([
+  { path: '/', element: <Homepage /> },
+  { path: '/products', element: <ProductsPage /> },
+]);
 
 function App() {
-  return <RouterProvider router={router} />; 
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Products.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductsPage = () => {
+  return <h1>The Products Page</h1>;
+};
+
+export default ProductsPage;
