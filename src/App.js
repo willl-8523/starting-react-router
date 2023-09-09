4560705281df14fed6e1b07ff6b1f6b8b3a626c1
willl85 <ngowilfried@gmail.com>
2023-09-09 16:24:18 +0200

    Add React Router package and create the first defiding route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Homepage from './pages/Home';
+
+const router = createBrowserRouter([{ path: '/', element: <Homepage /> }]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />; 
 }
 
 export default App;
