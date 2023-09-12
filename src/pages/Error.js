38bbcc7bcc4d5ecc469e65fec0fe6285a6ee8c75
willl85 <ngowilfried@gmail.com>
2023-09-12 13:47:09 +0200

    Remove the link to back to HomePage

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import MainNavigation from '../components/MainNavigation';
-import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
   return (
@@ -8,9 +7,7 @@ const ErrorPage = () => {
       <MainNavigation />
       <main>
         <h1>An error occurred!</h1>
-        <p>
-          Could not fing this page! <Link to="/">Go to Home page</Link>
-        </p>
+        <p>Could not fing this page!</p>
       </main>
     </>
   );
