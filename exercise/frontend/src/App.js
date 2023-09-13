64606f8cb3942680223a97002ae881a41b4fb933
willl85 <ngowilfried@gmail.com>
2023-09-13 14:08:51 +0200

    Output the ID of the selected event on the EventDetailPage

--- exercise/frontend/src/App.js
+++ exercise/frontend/src/App.js
@@ -1,7 +1,5 @@
 // Challenge / Exercise
 
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
