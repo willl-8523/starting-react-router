628b228ec315ebc7edc74e1be7dcdfffb2a83c6d
willl85 <ngowilfried@gmail.com>
2023-09-13 17:51:47 +0200

    The json() Utility Fonction

--- exercise/frontend/src/pages/Error.js
+++ exercise/frontend/src/pages/Error.js
@@ -5,25 +5,18 @@ import MainNavigation from '../components/MainNavigation';
 
 const ErrorPage = () => {
   const error = useRouteError();
-  console.log(error);
 
   let title = 'An error occurred!';
   let message = 'Something went wrong!';
 
-  if (error.data) {
-    if (error.status === 200) {
-      message = JSON.parse(error.data).message;
-    }
-
     if (error.status === 500) {
-      message = JSON.parse(error.data).message;
+      message = error.data.message;
     }
 
     if (error.status === 404) {
       title = 'Not found!';
       message = 'Could not find ressource or page.';
     }
-  }
 
   return (
     <>
