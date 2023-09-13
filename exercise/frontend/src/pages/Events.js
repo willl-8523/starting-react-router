547b219325d0c5e25412245d15c403cf097d47b2
willl85 <ngowilfried@gmail.com>
2023-09-13 16:40:26 +0200

    First way to handling with custom error

--- exercise/frontend/src/pages/Events.js
+++ exercise/frontend/src/pages/Events.js
@@ -3,6 +3,10 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
   const data = useLoaderData();
+
+  if (data.isError) {
+    return <p>{data.message}</p>
+  }
   const events = data.events;
 
   return <EventsList events={events} />;
@@ -14,7 +18,7 @@ export async function loader() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
-    // ...
+    return {isError: true, message: 'Could not fetch events.'}
   } else {
     return response;
   }
